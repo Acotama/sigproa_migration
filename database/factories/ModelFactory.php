f545e27a90b5876e715d3b999ba704b4f9945c8a
Javier <javier@local.dev>
2026-04-23 21:52:08 -0500

    chore: add Input alias compatibility and replace str_random helper

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -18,6 +18,6 @@ $factory->define(sayhuite\User::class, function (Faker\Generator $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'remember_token' => Illuminate\Support\Str::random(10),
     ];
 });
