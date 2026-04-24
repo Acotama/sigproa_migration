1f115d6c77246bb135cada89bcffd57f692e1aab
Javier <javier@local.dev>
2026-04-23 23:52:33 -0500

    chore: remove unused controllers and legacy views

--- routes/web_public.php
+++ routes/web_public.php
@@ -20,8 +20,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index');
 
-//Route::get('/hashimg', 'TService@hashimg');
-
 //Route::get('/console', 'PipTotalPrioriController@console');
 
 /*Route::get('/pusher', function(){
